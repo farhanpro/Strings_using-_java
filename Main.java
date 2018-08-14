49d8bd75bd98954c2c989147afbb609a21ca4ee8
farhanpro <31347917+farhanpro@users.noreply.github.com>
2018-08-15 00:06:42 +0530

    Add files via upload
    This is a must watch program

--- Main.java
+++ Main.java
@@ -1,17 +1,28 @@
-package com.company;
+package com.pluralsight;
 
 public class Main {
 
     public static void main(String[] args) {
-        int x = 1;
-        while (x < 3) {
-            System.out.print("Doo");
-            System.out.print("Bee");
-            x = x + 1;
-
-            if (x == 3) {
-                System.out.println("Do");
+        int x=0;
+        while (x<4 ){
+            System.out.print("a");
+            if(x<1){
+              System.out.print("an")  ;
             }
+            ;
+            if(x>1){
+                System.out.print(" ");
+
+                x=x+1;
+            }
+            if(x==1){
+                System.out.print("noise");
+            }
+            if(x<2){
+                System.out.print("oyster");
+            }
+            System.out.println("");
+            x+=1;
         }
     }
 }
